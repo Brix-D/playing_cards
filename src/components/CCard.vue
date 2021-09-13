a7d24482605c85618054e7ee9a1bc6b2dc61eb92
Brix-D <Shlyakhta4@gmail.com>
2021-09-13 16:40:34 +0400

    2nd commit

---
+++ src/components/CCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="card">
+    <div class="card__shirt">
+      <img :src="shirtImage" alt="рубашка" />
+    </div>
+    <div class="card__front">
+      <img :src="frontImage" alt="карта" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CCard",
+  props: {
+    shirtImage: {
+      type: String,
+      required: true,
+    },
+    frontImage: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.card {
+  &__shirt {
+  }
+
+  &__front {
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="home">
+    <div class="container">
+        <div v-for="i in 6" :key="i">s</div>
+    </div>
   </div>
 </template>
 
@@ -12,3 +15,11 @@ export default {
   },
 };
 </script>
+
+<style scoped lang="scss">
+.container {
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  column-gap: $gutter;
+}
+</style>
