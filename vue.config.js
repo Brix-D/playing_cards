e4a00b11db625c0bc4124fbf4b616b4f4ff5c410
Brix-D <Shlyakhta4@gmail.com>
2021-09-16 17:47:23 +0400

    config pp

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,11 @@
 module.exports = {
   lintOnSave: false,
+  publicPath: process.env.NODE_ENV === 'production' ? '/playing_cards/' : '/',
   css: {
     loaderOptions: {
       sass: {
-        prependData: `@import '@/scss/all.scss';`
-      }
-    }
-  }
+        prependData: "@import '@/scss/all.scss';",
+      },
+    },
+  },
 };
